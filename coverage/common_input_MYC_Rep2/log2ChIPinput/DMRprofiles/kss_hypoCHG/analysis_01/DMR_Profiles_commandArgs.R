523390e36f552aaccb315f7bb8aa2e2a220e3cf1
ajtock <andytock@gmail.com>
2018-08-29 10:35:15 +0100

    DMR profiles

--- coverage/common_input_MYC_Rep2/log2ChIPinput/DMRprofiles/kss_hypoCHG/analysis_01/DMR_Profiles_commandArgs.R
+++ coverage/common_input_MYC_Rep2/log2ChIPinput/DMRprofiles/kss_hypoCHG/analysis_01/DMR_Profiles_commandArgs.R
@@ -3,7 +3,7 @@
 # Profile mean coverage around DMRs and random loci
 
 # Usage via Condor submission system on node7:
-# csmit -m 20G -c 1 "Rscript DMR_Profiles_commandArgs.R /home/meiosis/ajt200/analysis/REC8_pooled/coverage/common_input_MYC_Rep2/log2ChIPinput/log2_REC8_HA_Rep1_ChIP_REC8_MYC_Rep2_input_norm_allchrs_coverage_coord_tab.bed REC8_HA_Rep1"
+# csmit -m 20G -c 1 "Rscript DMR_Profiles_commandArgs.R /home/ajt200/analysis/REC8_pooled/coverage/common_input_MYC_Rep2/log2ChIPinput/log2_REC8_HA_Rep1_ChIP_REC8_MYC_Rep2_input_norm_allchrs_coverage_coord_tab.bed REC8_HA_Rep1"
 
 # Source functions to be used in this script
 source("/projects/ajt200/Rfunctions/covMatrix_DNAmethMatrix_target_ranLoc.R")
@@ -16,7 +16,7 @@ args <- commandArgs(trailingOnly = T)
 covDatPath <- as.character(args[1])
 libName <- as.character(args[2])
 
-matDir <- "/home/meiosis/ajt200/analysis/REC8_pooled/coverage/common_input_MYC_Rep2/log2ChIPinput/DMRprofiles/kss_hypoCHG/analysis_01/matrices/"
+matDir <- "/home/ajt200/analysis/REC8_pooled/coverage/common_input_MYC_Rep2/log2ChIPinput/DMRprofiles/kss_hypoCHG/analysis_01/matrices/"
 
 # Chromosome definitions
 chrs <- c("Chr1","Chr2","Chr3","Chr4","Chr5")
@@ -28,7 +28,7 @@ pericenEnd <- c(18480000, 7540000, 16860000, 6850000, 15650000)
 genome <- toGRanges(data.frame(chrs, chrStart, chrLens))
 
 # Import DMRs as GRanges object
-DMRs <- read.table("/home/meiosis/ajt200/BS_Seq/Stroud_2013/DMRs/suvh456_hypoCHG_DMR_vs3reps_min4filter_mg200.bed", header = F)
+DMRs <- read.table("/home/ajt200/BS_Seq/Stroud_2013/DMRs/suvh456_hypoCHG_DMR_vs3reps_min4filter_mg200.bed", header = F)
 DMRsGR <- GRanges(seqnames = DMRs[,1],
                   ranges = IRanges(start = DMRs[,2], end = DMRs[,3]),
                   strand = "*")
