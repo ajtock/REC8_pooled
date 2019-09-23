70bfc85a09530b4f189c48607ece498cce887ac7
ajtock <andytock@gmail.com>
2019-09-23 17:13:27 +0100

    corrs

---
+++ coverage/common_input_MYC_Rep1/log2ChIPinput/genomeProfiles/noZscore/genomewide_correlations_at_given_scale.R
@@ -0,0 +1,54 @@
+#!/applications/R/R-3.5.0/bin/Rscript
+
+# Calculate genome-wide Spearman's rank-order correlations between libraries using
+#  windowed log2(ChIP/input) coverage values 
+
+winNames <- c("2kb", "5kb", "10kb", "20kb", "50kb", "100kb")
+
+ASY1_Rep1_list <- lapply(seq_along(winNames), function(x) {
+  read.table(paste0("ASY1_Rep1_ChIP_REC8_MYC_Rep1_input_genome_norm_coverage_",
+                    winNames[x], "_noZscore.tsv"),
+             header = T)[,6]
+})
+ASY1_Rep2_list <- lapply(seq_along(winNames), function(x) {
+  read.table(paste0("ASY1_Rep2_ChIP_REC8_MYC_Rep1_input_genome_norm_coverage_",
+                    winNames[x], "_noZscore.tsv"),
+             header = T)[,6]
+})
+REC8_HA_Rep1_list <- lapply(seq_along(winNames), function(x) {
+  read.table(paste0("REC8_HA_Rep1_ChIP_REC8_MYC_Rep1_input_genome_norm_coverage_",
+                    winNames[x], "_noZscore.tsv"),
+             header = T)[,6]
+})
+REC8_HA_Rep2_list <- lapply(seq_along(winNames), function(x) {
+  read.table(paste0("REC8_HA_Rep2_ChIP_REC8_MYC_Rep1_input_genome_norm_coverage_",
+                    winNames[x], "_noZscore.tsv"),
+             header = T)[,6]
+})
+REC8_MYC_Rep1_list <- lapply(seq_along(winNames), function(x) {
+  read.table(paste0("REC8_MYC_Rep1_ChIP_REC8_MYC_Rep1_input_genome_norm_coverage_",
+                    winNames[x], "_noZscore.tsv"),
+             header = T)[,6]
+})
+
+ASY1_Rep1_v_ASY1_Rep2 <- sapply(seq_along(winNames), function(x) {
+  cor.test(x = ASY1_Rep1_list[[x]],
+           y = ASY1_Rep2_list[[x]],
+           method = "spearman")
+})
+
+REC8_HA_Rep1_v_REC8_HA_Rep2 <- sapply(seq_along(winNames), function(x) {
+  cor(x = REC8_HA_Rep1_list[[x]],
+      y = REC8_HA_Rep2_list[[x]],
+      method = "spearman")
+})
+REC8_HA_Rep1_v_REC8_MYC_Rep1 <- sapply(seq_along(winNames), function(x) {
+  cor(x = REC8_HA_Rep1_list[[x]],
+      y = REC8_MYC_Rep1_list[[x]],
+      method = "spearman")
+})
+REC8_HA_Rep2_v_REC8_MYC_Rep1 <- sapply(seq_along(winNames), function(x) {
+  cor(x = REC8_HA_Rep2_list[[x]],
+      y = REC8_MYC_Rep1_list[[x]],
+      method = "spearman")
+})
