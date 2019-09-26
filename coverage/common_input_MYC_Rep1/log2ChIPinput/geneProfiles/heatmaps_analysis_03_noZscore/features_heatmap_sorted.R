ffff6f1496bd0eee0ca15172db2d6189ce91cb55
ajtock <andytock@gmail.com>
2019-09-26 14:15:53 +0100

    geneProfiles

---
+++ coverage/common_input_MYC_Rep1/log2ChIPinput/geneProfiles/heatmaps_analysis_03_noZscore/features_heatmap_sorted.R
@@ -0,0 +1,235 @@
+#!/applications/R/R-3.4.0/bin/Rscript
+
+# Plot heatmaps of features sorted by coverage levels between start and end sites
+
+# Usage:
+# /applications/R/R-3.4.0/bin/Rscript features_heatmap_sorted.R ASY1_Rep2 genes 2000 2kb '2 kb' 20 20bp bodies
+
+#libName <- "ASY1_Rep2"
+#featureName <- "genes"
+#upstream <- 2000
+#downstream <- 2000
+#flankName <- "2kb"
+#flankNamePlot <- "2 kb"
+#binSize <- 20
+#binName <- "20bp"
+#region <- "bodies"
+
+args <- commandArgs(trailingOnly = T)
+libName <- args[1]
+featureName <- args[2]
+upstream <- as.numeric(args[3])
+downstream <- as.numeric(args[3])
+flankName <- args[4]
+flankNamePlot <- args[5]
+binSize <- as.numeric(args[6])
+binName <- args[7]
+region <- args[8]
+
+library(EnrichedHeatmap)
+library(parallel)
+library(circlize)
+library(RColorBrewer)
+library(tidyr)
+library(ggplot2)
+library(doParallel)
+
+matDir <- "./matrices/"
+plotDir <- "./plots/"
+regionPlotDir <- paste0(plotDir, region,
+                        "_by_", libName, "/")
+system(paste0("[ -d ", plotDir, " ] || mkdir ", plotDir))
+system(paste0("[ -d ", regionPlotDir, " ] || mkdir ", regionPlotDir))
+plotDir <- regionPlotDir
+
+ChIPseqNames <- c(
+                  "ASY1_Rep2",
+                  "REC8_HA_Rep2",
+                  "SPO11_1_oligos_RPI1",
+                  "MTOPVIB_HA_Rep1",
+                  "MTOPVIB_HA_Rep2",
+                  "MNase"
+                 )
+ChIPseqNamesPlot <- c(
+                      "ASY1",
+                      "REC8-HA",
+                      "SPO11-1-oligos",
+                      "MTOPVIB Rep1",
+                      "MTOPVIB Rep2",
+                      "MNase"
+                     )
+ChIPseqColours <- c(
+                    "red",
+                    "green2",
+                    "dodgerblue2",
+                    "darkcyan",
+                    "darkcyan",
+                    "purple4"
+                   )
+
+# Load matrix and extract region for sorting of features
+mat1 <- as.matrix(read.table(paste0(matDir,
+                                    libName,
+                                    "_norm_cov_feature_smoothed_target_and_",
+                                    flankName, "_flank_dataframe.txt"),
+                             header = T))
+bodyLength <- (dim(mat1)[2]-((upstream+downstream)/binSize))*binSize
+if( region == "promoters" ) {
+  mat1Region <- mat1[,(((upstream-500)/binSize)+1):(upstream/binSize)]
+} else if ( region == "terminators" ) {
+  mat1Region <- mat1[,(((upstream+bodyLength)/binSize)+1):(((upstream+bodyLength)/binSize)+(500/binSize))]
+} else if ( region == "bodies" ) {
+  mat1Region <- mat1[,((upstream/binSize)+1):((upstream+bodyLength)/binSize)]
+} else {
+  print("The region name provided does not match 'promoters', 'terminators', or 'bodies'")
+}
+mat1RegionRowMeans <- rowMeans(mat1Region, na.rm = T)
+mat1RegionRowMeansSorted <- sort.int(mat1RegionRowMeans,
+                                     decreasing = T,
+                                     index.return = T,
+                                     na.last = T)
+mat1RegionRowSums <- rowSums(mat1Region, na.rm = T)
+mat1RegionRowSumsSorted <- sort.int(mat1RegionRowSums,
+                                    decreasing = T,
+                                    index.return = T,
+                                    na.last = T)
+mat1RegionRowMedians <- apply(X = mat1Region,
+                              MARGIN = 1,
+                              FUN = median)
+mat1RegionRowMediansSorted <- sort.int(mat1RegionRowMedians,
+                                       decreasing = T,
+                                       index.return = T,
+                                       na.last = T)
+
+# Load feature coverage matrix for each ChIP-seq dataset
+mats <- mclapply(seq_along(ChIPseqNames), function(x) {
+  as.matrix(read.table(paste0(matDir,
+                              ChIPseqNames[x],                            
+                              "_norm_cov_feature_smoothed_target_and_",
+                              flankName, "_flank_dataframe.txt"),
+                       header = T))
+}, mc.cores = length(ChIPseqNames))
+
+# Sort by decreasing mat1RegionRowMeans
+matsSorted <- mclapply(seq_along(mats), function(x) {
+  mats[[x]][sort.int(mat1RegionRowMeans,
+                     decreasing = T,
+                     index.return = T,
+                     na.last = T)$ix,]
+}, mc.cores = length(ChIPseqNames))
+
+# Convert matrices to class "normalizedMatrix" with associated attributes
+# for use by EnrichedHeatmap function
+for(x in seq_along(matsSorted)) {
+  attr(matsSorted[[x]], "upstream_index")         = 1:(upstream/binSize)
+  attr(matsSorted[[x]], "target_index")           = ((upstream/binSize)+1):((upstream+bodyLength)/binSize)
+  attr(matsSorted[[x]], "downstream_index")       = (((upstream+bodyLength)/binSize)+1):(((upstream+bodyLength)/binSize)+(downstream/binSize))
+  attr(matsSorted[[x]], "extend")                 = c(upstream, downstream)
+  attr(matsSorted[[x]], "smooth")                 = TRUE
+  attr(matsSorted[[x]], "signal_name")            = ChIPseqNamesPlot[x]
+  attr(matsSorted[[x]], "target_name")            = featureName
+  attr(matsSorted[[x]], "target_is_single_point") = FALSE
+  attr(matsSorted[[x]], "background")             = 0
+  attr(matsSorted[[x]], "signal_is_categorical")  = FALSE
+  class(matsSorted[[x]]) = c("normalizedMatrix", "matrix")
+}
+
+if(featureName == "genes") {
+  featureStartLab <- "TSS"
+  featureEndLab <- "TTS"
+} else {
+  featureStartLab <- "Start"
+  featureEndLab <- "End"
+}
+
+# Heatmap plotting function
+# Note that for plotting heatmaps for individual datasets in separate PDFs,
+# must edit this function - print(EnrichedHeatmap(...))
+featureHeatmap <- function(matSorted,
+                           col_fun,
+                           colour,
+                           datName,
+                           rowOrder) {
+  EnrichedHeatmap(mat = matSorted,
+                  col = col_fun,
+                  row_order = rowOrder,
+                  column_title = datName,
+                  top_annotation = HeatmapAnnotation(enriched = anno_enriched(gp = gpar(col = colour,
+                                                                                        lwd = 3),
+                                                                              yaxis_side = "right",
+                                                                              yaxis_facing = "right",
+                                                                              yaxis_gp = gpar(fontsize = 10),
+                                                                              pos_line_gp = gpar(col = "black",
+                                                                                                 lty = 2,
+                                                                                                 lwd = 2))),
+                  top_annotation_height = unit(2, "cm"),
+                  width = unit(6, "cm"),
+                  heatmap_legend_param = list(title = datName,
+                                              title_position = "topcenter",
+                                              title_gp = gpar(font = 2, fontsize = 12),
+                                              legend_direction = "horizontal",
+                                              labels_gp = gpar(fontsize = 10)),
+                  axis_name = c(paste0("-", flankNamePlot),
+                                featureStartLab, featureEndLab,
+                                paste0("+", flankNamePlot)),
+                  axis_name_gp = gpar(fontsize = 14),
+                  border = FALSE,
+                  pos_line_gp = gpar(col = "white", lty = 2, lwd = 2),
+                  # If converting into png with pdfTotiffTopng.sh,
+                  # set use_raster to FALSE
+                  use_raster = FALSE)
+                  #use_raster = TRUE, raster_device = "png", raster_quality = 4)
+}
+
+# Define heatmap colours
+rich8to6equal <- c("#000041", "#0000CB", "#0081FF", "#FDEE02", "#FFAB00", "#FF3300")
+
+library(doParallel)
+registerDoParallel(cores = length(ChIPseqNames))
+print("Currently registered parallel backend name, version and cores")
+print(getDoParName())
+print(getDoParVersion())
+print(getDoParWorkers())
+
+# Plot together
+# ChIP-seq
+pdf(paste0(plotDir, paste0(ChIPseqNames, collapse = "_"), "_around_", featureName,
+           "_heatmaps_ordered_by_", libName, "_in_", region, ".pdf"),
+    width = 3*length(ChIPseqNames), height = 8)
+# Doesn't work
+#par(mfrow = c(2, length(ChIPseqNames)/2))
+htmps <- NULL
+for(x in 1:length(ChIPseqNames)) {
+  ChIPseq_col_fun <- colorRamp2(quantile(matsSorted[[x]],
+                                         c(0.5, 0.6, 0.7, 0.8, 0.9, 0.95),
+                                         na.rm = T),
+                                rich8to6equal)
+  htmp <- featureHeatmap(matSorted = matsSorted[[x]],
+                         col_fun = ChIPseq_col_fun,
+                         colour = ChIPseqColours[x],
+                         datName = ChIPseqNamesPlot[x],
+                         rowOrder = c(1:dim(matsSorted[[x]])[1]))
+  htmps <- htmps + htmp
+}
+draw(htmps,
+     heatmap_legend_side = "bottom",
+     gap = unit(c(12), "mm"))
+dev.off()
+
+# Plot individually
+# ChIP-seq
+#foreach(x = 1:length(ChIPseqNames)) %dopar% {
+#  ChIPseq_col_fun <- colorRamp2(quantile(matsSorted[[x]],
+#                                         c(0.5, 0.6, 0.7, 0.8, 0.9, 0.95),
+#                                         na.rm = T),
+#                                rich8to6equal)
+#  pdf(paste0(plotDir, ChIPseqNames[x], "_around_", featureName,
+#             "_heatmap_ordered_by_", libName, "_in_", region, ".pdf"),
+#      width = 5, height = 10)
+#  featureHeatmap(matSorted = matsSorted[[x]],
+#                 col_fun = ChIPseq_col_fun,
+#                 colour = "red",
+#                 datName = ChIPseqNamesPlot[x],
+#                 rowOrder = c(1:dim(matsSorted[[x]])[1]))
+#  dev.off()
+#}
---
+++ coverage/common_input_MYC_Rep1/log2ChIPinput/geneProfiles/heatmaps_analysis_03_noZscore/gene_Profiles_commandArgs.R
@@ -0,0 +1,79 @@
+#!/applications/R/R-3.4.0/bin/Rscript
+
+# Profile mean coverage around genes and random loci
+
+# Usage via Condor submission system on node7:
+# csmit -m 20G -c 1 "./gene_Profiles_commandArgs.R 2000 2kb 20 /home/ajt200/analysis/REC8_pooled/coverage/REC8_MYC_Rep1_ChIP_norm_allchrs_coverage_coord_tab.bed REC8_MYC_Rep1"
+
+# Source functions to be used in this script
+source("/projects/ajt200/Rfunctions/covMatrix_DNAmethMatrix_target_ranLoc_R3.4.0.R")
+
+library(EnrichedHeatmap)
+library(genomation)
+library(regioneR)
+
+args <- commandArgs(trailingOnly = T)
+flankSize <- as.numeric(args[1])
+flankName <- as.character(args[2])
+winSize <- as.numeric(args[3])
+covDatPath <- as.character(args[4])
+libName <- as.character(args[5])
+
+matDir <- "./matrices/"
+plotDir <- "./plots/"
+system(paste0("[ -d ", matDir, " ] || mkdir ", matDir))
+system(paste0("[ -d ", plotDir, " ] || mkdir ", plotDir))
+
+# Chromosome definitions
+chrs <- c("Chr1", "Chr2", "Chr3", "Chr4", "Chr5")
+chrStart <- c(rep(1, 5))
+chrLens <- c(30427671, 19698289, 23459830, 18585056, 26975502)
+centromeres <- c(15086045, 3607929, 13587786, 3956021, 11725024)
+pericenStart <- c(11330001, 990001, 10200001, 990001, 8890001)
+pericenEnd <- c(18480000, 7540000, 16860000, 6850000, 15650000)
+genome <- toGRanges(data.frame(chrs, chrStart, chrLens))
+
+# Import genes as GRanges object
+representive_genes_uniq <- system("ls /projects/ajt200/TAIR10/representative_genes/representative_genes_uniq_fmt_strand.txt", intern=T)
+genesGR <- readGeneric(representive_genes_uniq, header = T, strand = 4, meta.col = list(gene_model = 5))
+seqlevels(genesGR) <- sub("", "Chr", seqlevels(genesGR))
+print("***********genes***********")
+print(genesGR)
+# Generate GRanges object containing random loci of same number and
+# size distribution as genesGR
+set.seed(856291)
+ranLocGR <- randomizeRegions(genesGR,
+                             genome = genome,
+                             per.chromosome = TRUE,
+                             allow.overlaps = TRUE)
+
+# Specify locations of normalised per base coverage files
+libPath <- system(paste0("ls ", covDatPath), intern = T)
+
+# Import coverage files as GRanges objects and assign to library names
+covGR <- readGeneric(libPath, meta.col = list(coverage = 4))
+assign(paste0(libName), covGR)
+
+# Define matrix and column mean coverage outfile (mean profiles)
+outDF <- list(paste0(matDir, libName,
+                     "_norm_cov_feature_smoothed_target_and_", flankName, "_flank_dataframe.txt"),
+              paste0(matDir, libName,
+                     "_norm_cov_ranLoc_smoothed_target_and_", flankName, "_flank_dataframe.txt"))
+outDFcolMeans <- list(paste0(matDir, libName,
+                             "_norm_cov_feature_smoothed_target_and_", flankName, "_flank_dataframe_colMeans.txt"),
+                      paste0(matDir, libName,
+                             "_norm_cov_ranLoc_smoothed_target_and_", flankName, "_flank_dataframe_colMeans.txt"))
+
+# Run covMatrix() function on each coverage GRanges object to obtain matrices
+## containing normalised coverage values around target and random loci
+covMatrix(signal = covGR,
+          feature = genesGR,
+          ranLoc = ranLocGR,
+          featureSize = mean(width(genesGR)),
+          flankSize = flankSize,
+          winSize = winSize,
+          outDF = outDF,
+          outDFcolMeans = outDFcolMeans)
+print(paste0(libName, " profile calculation complete"))
+
+
