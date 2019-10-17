79bcb5e823046fa3cbc0d5db2dd8aa08438390ec
ajtock <andytock@gmail.com>
2019-10-17 08:12:57 +0100

    correlations at scales

--- coverage/common_input_MYC_Rep1/log2ChIPinput/genomeProfiles/noZscore/genomewide_correlations_at_given_scale.R
+++ coverage/common_input_MYC_Rep1/log2ChIPinput/genomeProfiles/noZscore/genomewide_correlations_at_given_scale.R
@@ -32,6 +32,11 @@ REC8_MYC_Rep1_list <- lapply(seq_along(winNames), function(x) {
                     winNames[x], "_noZscore.tsv"),
              header = T)[,6]
 })
+kss_REC8_HA_Rep1_list <- lapply(seq_along(winNames), function(x) {
+  read.table(paste0("kss_REC8_HA_Rep1_ChIP_kss_REC8_HA_Rep1_input_genome_norm_coverage_",
+                    winNames[x], "_noZscore.tsv"),
+             header = T)[,6]
+})
 
 ASY1_Rep1_v_ASY1_Rep2 <- sapply(seq_along(winNames), function(x) {
   cor(x = ASY1_Rep1_list[[x]],
@@ -81,21 +86,50 @@ REC8_HA_Rep1_v_REC8_MYC_Rep1 <- sapply(seq_along(winNames), function(x) {
       y = REC8_MYC_Rep1_list[[x]],
       method = "spearman")
 })
+REC8_HA_Rep1_v_kss_REC8_HA_Rep1 <- sapply(seq_along(winNames), function(x) {
+  cor(x = REC8_HA_Rep1_list[[x]],
+      y = kss_REC8_HA_Rep1_list[[x]],
+      method = "spearman")
+})
 # REC8_HA_Rep2
 REC8_HA_Rep2_v_REC8_MYC_Rep1 <- sapply(seq_along(winNames), function(x) {
   cor(x = REC8_HA_Rep2_list[[x]],
       y = REC8_MYC_Rep1_list[[x]],
       method = "spearman")
 })
+REC8_HA_Rep2_v_kss_REC8_HA_Rep1 <- sapply(seq_along(winNames), function(x) {
+  cor(x = REC8_HA_Rep2_list[[x]],
+      y = kss_REC8_HA_Rep1_list[[x]],
+      method = "spearman")
+})
+# REC8_MYC_Rep1
+REC8_MYC_Rep1_v_kss_REC8_HA_Rep1 <- sapply(seq_along(winNames), function(x) {
+  cor(x = REC8_MYC_Rep1_list[[x]],
+      y = kss_REC8_HA_Rep1_list[[x]],
+      method = "spearman")
+})
 
-corrDF <- data.frame(ASY1_Rep1_v_ASY1_Rep2 = round(ASY1_Rep1_v_ASY1_Rep2, digits = 2),
-                     ASY1_Rep1_v_REC8_HA_Rep1 = round(ASY1_Rep1_v_REC8_HA_Rep1, digits = 2),
-                     ASY1_Rep1_v_REC8_HA_Rep2 = round(ASY1_Rep1_v_REC8_HA_Rep2, digits = 2),
-                     ASY1_Rep1_v_REC8_MYC_Rep1 = round(ASY1_Rep1_v_REC8_MYC_Rep1, digits = 2),
-                     ASY1_Rep2_v_REC8_HA_Rep1 = round(ASY1_Rep2_v_REC8_HA_Rep1, digits = 2),
-                     ASY1_Rep2_v_REC8_HA_Rep2 = round(ASY1_Rep2_v_REC8_HA_Rep2, digits = 2),
-                     ASY1_Rep2_v_REC8_MYC_Rep1 = round(ASY1_Rep2_v_REC8_MYC_Rep1, digits = 2),
+#corrDF <- data.frame(ASY1_Rep1_v_ASY1_Rep2 = round(ASY1_Rep1_v_ASY1_Rep2, digits = 2),
+#                     ASY1_Rep1_v_REC8_HA_Rep1 = round(ASY1_Rep1_v_REC8_HA_Rep1, digits = 2),
+#                     ASY1_Rep1_v_REC8_HA_Rep2 = round(ASY1_Rep1_v_REC8_HA_Rep2, digits = 2),
+#                     ASY1_Rep1_v_REC8_MYC_Rep1 = round(ASY1_Rep1_v_REC8_MYC_Rep1, digits = 2),
+#                     ASY1_Rep2_v_REC8_HA_Rep1 = round(ASY1_Rep2_v_REC8_HA_Rep1, digits = 2),
+#                     ASY1_Rep2_v_REC8_HA_Rep2 = round(ASY1_Rep2_v_REC8_HA_Rep2, digits = 2),
+#                     ASY1_Rep2_v_REC8_MYC_Rep1 = round(ASY1_Rep2_v_REC8_MYC_Rep1, digits = 2),
+#                     REC8_HA_Rep1_v_REC8_HA_Rep2 = round(REC8_HA_Rep1_v_REC8_HA_Rep2, digits = 2),
+#                     REC8_HA_Rep1_v_REC8_MYC_Rep1 = round(REC8_HA_Rep1_v_REC8_MYC_Rep1, digits = 2),
+#                     REC8_HA_Rep2_v_REC8_MYC_Rep1 = round(REC8_HA_Rep2_v_REC8_MYC_Rep1, digits = 2))
+
+corrDF <- data.frame(
                      REC8_HA_Rep1_v_REC8_HA_Rep2 = round(REC8_HA_Rep1_v_REC8_HA_Rep2, digits = 2),
                      REC8_HA_Rep1_v_REC8_MYC_Rep1 = round(REC8_HA_Rep1_v_REC8_MYC_Rep1, digits = 2),
-                     REC8_HA_Rep2_v_REC8_MYC_Rep1 = round(REC8_HA_Rep2_v_REC8_MYC_Rep1, digits = 2))
+                     REC8_HA_Rep1_v_kss_REC8_HA_Rep1 = round(REC8_HA_Rep1_v_kss_REC8_HA_Rep1, digits = 2),
+                     REC8_HA_Rep2_v_REC8_MYC_Rep1 = round(REC8_HA_Rep2_v_REC8_MYC_Rep1, digits = 2),
+                     REC8_HA_Rep2_v_kss_REC8_HA_Rep1 = round(REC8_HA_Rep2_v_kss_REC8_HA_Rep1, digits = 2),
+                     REC8_MYC_Rep1_v_kss_REC8_HA_Rep1 = round(REC8_MYC_Rep1_v_kss_REC8_HA_Rep1, digits = 2)
+                    )
+write.table(corrDF,
+            file = "REC8_library_spearman_correlations_at_varying_scales.tsv",
+            row.names = F, sep = "\t", quote = F)
 
+                     
